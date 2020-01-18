24235882fabfaf4620d23b7fbfb5f1e2830de13d
clinton wambugu <clintonwambugu@gmail.com>
2020-01-19 00:25:01 +0300

    created a game class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,11 @@
- class Board 
+class Board 
   def initialize
    @board = [0,1,2,3,4,5,6,7,8]
   end
   
   def display_board
    puts "|#{@board[0]} | #{@board[1]} | #{@board[2]} |\n............\n|#{@board[3]} | #{@board[4]} | #{@board[5]} |\n............\n|#{@board[6]} | #{@board[7]} | #{@board[8]} |"  
-  end
+   end
   
    def modify_board(position,symbol)
      @board[position] = symbol
@@ -15,7 +15,7 @@
  
  class Player
   @@draw = 0
-  attr_reader :name,:position
+  attr_reader :name
   
   def initialize(name)
    @name = name
@@ -30,22 +30,43 @@
   end
  end
  
- game = true
+ class Game 
+   def initialize(player1,player2,board)
+   @player1 = player1
+   @player2 = player2
+   @board = board
+   @current_player = @player1
+  end
  
- def play
-  board = Board.new
-  board.display_board
-  puts "you are playing as X choose position: "
-  player_x = Player.create_player('X')
-  player_x_choice = gets.chomp.to_i
-  board.modify_board(player_x_choice,player_x.name)
-  puts "you are playing as O choose position: "
-  player_o = Player.create_player('o')
-  player_o_choice = gets.chomp.to_i
-  board.modify_board(player_o_choice,player_o.name)
+   def winner?
+   #check winner
+   end
+   
+  def change_current_player
+   @current_player = (@current_player == @player1 ? @player2 : @player1)
+  end
+ 
+   def take_move
+     puts " choose a position 0..8 "
+     move = gets.chomp.to_i
+     @board.modify_board(move,@current_player.name)
+     #change_current_player
+   end
+ 
+  def play
+   @board.display_board
+   until winner?
+     take_move
+     change_current_player
+   end
+  end
  end
  
- while game
-  play
-  game = false
- end
+  player_x = Player.create_player("X")
+  player_o = Player.create_player("O")
+  game_board = Board.new
+  game = Game.new(player_x,player_o,game_board)
+  game.play
+ 
+  
+ 
